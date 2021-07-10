c13e6d768071dc0ad9df2f1072d925634f2d4350
PedroLucasFB <80292645+PedroLucasFB@users.noreply.github.com>
2021-07-10 14:57:44 -0300

    Add files via upload
    última versão Chat, com login usando o Facebook.

---
+++ src/components/ChatIntro.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './ChatIntro.css';
+
+export default () => {
+    return (
+        <div className="chatIntro">
+            <h1>Sem mensagens no momento</h1>
+            <h2>Utilize este chat para uma boa comunicação entre Profissional e Cliente</h2>
+        </div>
+    );
+}
---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyDAKnqFm6K7nkCene2QExDnA8FxQEe3d_I",
+    authDomain: "azozap-a93c9.firebaseapp.com",
+    projectId: "azozap-a93c9",
+    storageBucket: "azozap-a93c9.appspot.com",
+    messagingSenderId: "593451694193",
+    appId: "1:593451694193:web:1bfb0ef5eb42f29cf503d0"
+};
